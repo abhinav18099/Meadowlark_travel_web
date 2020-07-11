4d807a9793b151d7e10bfaf35bc568b0031c5e76
Abhinav <mehtaabhinav567@gmail.com>
2020-07-11 16:08:35 +0530

    email functionality added

--- meadowlark.js
+++ meadowlark.js
@@ -1,11 +1,13 @@
-var express = require('express');
-var fortune = require('./lib/fortune.js');
-var formidable = require('formidable');
-var jqupload = require('jquery-file-upload-middleware');
-var credentials = require('./credentials.js');
-
+var express = require('express'),
+	fortune = require('./lib/fortune.js'),
+	formidable = require('formidable');
 
 var app = express();
+
+var credentials = require('./credentials.js');
+
+var emailService = require('./lib/email.js')(credentials);
+
 // set up handlebars view engine
 var handlebars = require('express-handlebars').create({
     defaultLayout:'main',
@@ -22,8 +24,23 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+}));
 app.use(express.static(__dirname + '/public'));
 app.use(require('body-parser')());
+
+// flash message middleware
+app.use(function(req, res, next){
+	// if there's a flash message, transfer
+	// it to the context, then clear it
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
+	next();
+});
 
 // set 'showTests' context property if the querystring contains test=1
 app.use(function(req, res, next){
@@ -32,6 +49,7 @@ app.use(function(req, res, next){
 	next();
 });
 
+// mocked weather data
 function getWeatherData(){
     return {
         locations: [
@@ -60,24 +78,11 @@ function getWeatherData(){
     };
 }
 
-app.use(function(req,res,next){
+// middleware to add weather data to context
+app.use(function(req, res, next){
 	if(!res.locals.partials) res.locals.partials = {};
-	res.locals.partials.weatherContext = getWeatherData();
-	next();
-});
-
-app.use(require('cookie-parser')(credentials.cookieSecret));
-
-app.use('/upload', function(req,res,next){
-    var now = Date();
-    jpupload.fileHandler({
-        uploadDir:function(){
-            return __dirname+'/public/uploads/'+ now;
-        },
-        UploadUrl: function(){
-            return '/uploads/' + now;
-        },
-    })(req,res,next);
+ 	res.locals.partials.weatherContext = getWeatherData();
+ 	next();
 });
 
 app.get('/', function(req, res) {
@@ -89,25 +94,12 @@ app.get('/about', function(req,res){
 		pageTestScript: '/qa/tests-about.js' 
 	} );
 });
-app.get('/tours/hood-river', function(req, res){
-	res.render('tours/hood-river');
-});
-app.get('/tours/oregon-coast', function(req, res){
-	res.render('tours/oregon-coast');
-});
 app.get('/tours/request-group-rate', function(req, res){
 	res.render('tours/request-group-rate');
 });
-
-app.get("/headers",function(req,res){
-	res.set('Content-Type','text/plain');
-	res.send(req.acceptsLanguages());
-});
-
-app.get("/jquery-test",function(req,res){
+app.get('/jquery-test', function(req, res){
 	res.render('jquery-test');
 });
-
 app.get('/nursery-rhyme', function(req, res){
 	res.render('nursery-rhyme');
 });
@@ -119,43 +111,203 @@ app.get('/data/nursery-rhyme', function(req, res){
 		noun: 'heck',
 	});
 });
-
 app.get('/thank-you', function(req, res){
-    res.cookie('monster','nom nom')
 	res.render('thank-you');
 });
 app.get('/newsletter', function(req, res){
-    // we will learn about CSRF later...for now, we just
-    // provide a dummy value
-    res.render('newsletter', { csrf: 'CSRF token goes here' });
-});
-app.post('/process', function(req, res){
-    if(req.xhr || req.accepts('json,html')==='json'){
-        // if there were an error, we would send { error: 'error description' }
-        res.send({ success: true });
-    } else {
-        // if there were an error, we would redirect to an error page
-        res.redirect(303, '/thank-you');
-    }
+	res.render('newsletter');
 });
 
-app.get('/contest/vacation-photo',function(req,res){
+// for now, we're mocking NewsletterSignup:
+function NewsletterSignup(){
+}
+NewsletterSignup.prototype.save = function(cb){
+	cb();
+};
+
+// mocking product database
+function Product(){
+}
+Product.find = function(conditions, fields, options, cb){
+	if(typeof conditions==='function') {
+		cb = conditions;
+		conditions = {};
+		fields = null;
+		options = {};
+	} else if(typeof fields==='function') {
+		cb = fields;
+		fields = null;
+		options = {};
+	} else if(typeof options==='function') {
+		cb = options;
+		options = {};
+	}
+	var products = [
+		{
+			name: 'Hood River Tour',
+			slug: 'hood-river',
+			category: 'tour',
+			maximumGuests: 15,
+			sku: 723,
+		},
+		{
+			name: 'Oregon Coast Tour',
+			slug: 'oregon-coast',
+			category: 'tour',
+			maximumGuests: 10,
+			sku: 446,
+		},
+		{
+			name: 'Rock Climbing in Bend',
+			slug: 'rock-climbing/bend',
+			category: 'adventure',
+			requiresWaiver: true,
+			maximumGuests: 4,
+			sku: 944,
+		}
+	];
+	cb(null, products.filter(function(p) {
+		if(conditions.category && p.category!==conditions.category) return false;
+		if(conditions.slug && p.slug!==conditions.slug) return false;
+		if(isFinite(conditions.sku) && p.sku!==Number(conditions.sku)) return false;
+		return true;
+	}));
+};
+Product.findOne = function(conditions, fields, options, cb){
+	if(typeof conditions==='function') {
+		cb = conditions;
+		conditions = {};
+		fields = null;
+		options = {};
+	} else if(typeof fields==='function') {
+		cb = fields;
+		fields = null;
+		options = {};
+	} else if(typeof options==='function') {
+		cb = options;
+		options = {};
+	}
+	Product.find(conditions, fields, options, function(err, products){
+		cb(err, products && products.length ? products[0] : null);
+	});
+};
+
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function(req, res){
+	var name = req.body.name || '', email = req.body.email || '';
+	// input validation
+	if(!email.match(VALID_EMAIL_REGEX)) {
+		if(req.xhr) return res.json({ error: 'Invalid name email address.' });
+		req.session.flash = {
+			type: 'danger',
+			intro: 'Validation error!',
+			message: 'The email address you entered was  not valid.',
+		};
+		return res.redirect(303, '/newsletter/archive');
+	}
+	new NewsletterSignup({ name: name, email: email }).save(function(err){
+		if(err) {
+			if(req.xhr) return res.json({ error: 'Database error.' });
+			req.session.flash = {
+				type: 'danger',
+				intro: 'Database error!',
+				message: 'There was a database error; please try again later.',
+			};
+			return res.redirect(303, '/newsletter/archive');
+		}
+		if(req.xhr) return res.json({ success: true });
+		req.session.flash = {
+			type: 'success',
+			intro: 'Thank you!',
+			message: 'You have now been signed up for the newsletter.',
+		};
+		return res.redirect(303, '/newsletter/archive');
+	});
+});
+app.get('/newsletter/archive', function(req, res){
+	res.render('newsletter/archive');
+});
+app.get('/contest/vacation-photo', function(req, res){
     var now = new Date();
-    res.render('contest/vacation-photo',{
-        year: now.getFullYear(),month: now.getMonth()
-    });
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
 });
-
-app.post('/contest/vacation-photo/:year/:month',function(req,res){
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
     var form = new formidable.IncomingForm();
-    form.parse(req,function(err,fields,files){
-        if(err) return res.redirect(303,'/error');
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
         console.log('received fields:');
         console.log(fields);
         console.log('received files:');
         console.log(files);
-        res.redirect(303,'/thank-you');
+        res.redirect(303, '/thank-you');
     });
+});
+app.get('/tours/:tour', function(req, res, next){
+	Product.findOne({ category: 'tour', slug: req.params.tour }, function(err, tour){
+		if(err) return next(err);
+		if(!tour) return next();
+		res.render('tour', { tour: tour });
+	});
+});
+app.get('/adventures/:subcat/:name', function(req, res, next){
+	Product.findOne({ category: 'adventure', slug: req.params.subcat + '/' + req.params.name  }, function(err, adventure){
+		if(err) return next(err);
+		if(!adventure) return next();
+		res.render('adventure', { adventure: adventure });
+	});
+});
+
+var cartValidation = require('./lib/cartValidation.js');
+
+app.use(cartValidation.checkWaivers);
+app.use(cartValidation.checkGuestCounts);
+
+app.post('/cart/add', function(req, res, next){
+	var cart = req.session.cart || (req.session.cart = { items: [] });
+	Product.findOne({ sku: req.body.sku }, function(err, product){
+		if(err) return next(err);
+		if(!product) return next(new Error('Unknown product SKU: ' + req.body.sku));
+		cart.items.push({
+			product: product,
+			guests: req.body.guests || 0,
+		});
+		res.redirect(303, '/cart');
+	});
+});
+app.get('/cart', function(req, res, next){
+	var cart = req.session.cart;
+	res.render('cart', { cart: cart });
+});
+app.get('/cart/checkout', function(req, res, next){
+	var cart = req.session.cart;
+	res.render('cart-checkout');
+});
+app.get('/cart/thank-you', function(req, res){
+	res.render('cart-thank-you', { cart: req.session.cart });
+});
+app.get('/email/cart/thank-you', function(req, res){
+	res.render('email/cart-thank-you', { cart: req.session.cart, layout: null });
+});
+app.post('/cart/checkout', function(req, res){
+	var cart = req.session.cart;
+	var name = req.body.name || '', email = req.body.email || '';
+	// input validation
+	if(!email.match(VALID_EMAIL_REGEX)) return res.next(new Error('Invalid email address.'));
+	// assign a random cart ID; normally we would use a database ID here
+	cart.billing = {
+		name: name,
+		email: email,
+	};
+    res.render('email/cart-thank-you', 
+    	{ layout: null, cart: cart }, function(err,html){
+	        if( err ) console.log('error in email template');
+	        emailService.send(cart.billing.email,
+	        	'Thank you for booking your trip with Meadowlark Travel!',
+	        	html);
+	    }
+    );
+    res.render('cart-thank-you', { cart: cart });
 });
 
 // 404 catch-all handler (middleware)
